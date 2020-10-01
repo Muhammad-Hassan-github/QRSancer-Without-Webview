6d3e0a164b0fdcdbb3bff59d22a997f0368596ee
Muhammad Hassan <mhassan258182211@gmail.com>
2020-10-01 15:56:53 +0500

    Designing compelete

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { View, StyleSheet, TouchableOpacity, Image, StatusBar } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Image, StatusBar ,ImageBackground } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Container, Header, Content, Button, Text ,Icon} from 'native-base';
@@ -9,19 +9,21 @@ import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-nat
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import QRScan from "./components/QRScan"
 import ProductDetails from "./components/productDetails"
+import backgroundImage from "./images/background1.jpg"
 
 function HomeScreen({ navigation }) {
   return (
+         <ImageBackground source={backgroundImage} style={style.image}>
     <View style={style.container}>
-      <StatusBar barStyle="default" backgroundColor="brown" />
+      <StatusBar barStyle="default" backgroundColor="#5a55af" />
       <Image
         style={{ height: hp("20%"), width: wp("40%") }}
         resizeMode="cover"
         source={require('./images/logo.png')}
       />
-      <View><Button large success style={style.VerifyBtn} onPress={() => navigation.navigate('QRScan')} ><Text> Verify </Text></Button></View>
-     
+      <View><Button    style={style.VerifyBtn} onPress={() => navigation.navigate('QRScan')} ><Text style={{fontSize:22}}> Verify </Text></Button></View>
     </View>
+      </ImageBackground>
   );
 }
 
@@ -62,14 +64,24 @@ const style = StyleSheet.create({
   container: {
     marginTop: RFPercentage(-10),
     flex: 1,
+    flexDirection: "column",
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: "brown"
+    
   },
   VerifyBtn: {
     marginTop: RFPercentage(10),
+    height:50,
+    width:150,
+    display:"flex",
+    justifyContent:"center"
 
 
-  }
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
+  },
 
 })
--- components/productDetails.js
+++ components/productDetails.js
@@ -33,11 +33,16 @@
 
 
 import React, { Component } from "react";
-import { Container, Header, Content, Text, Button, Toast, Root } from "native-base";
-import { Linking, TouchableOpacity, Alert, View, Dimensions, StyleSheet } from "react-native"
+import { Container, Header, Content, Text, Button, Toast, Root, } from "native-base";
+import { Linking, TouchableOpacity, Alert, View, Dimensions, StyleSheet, ImageBackground } from "react-native"
 import { useHeaderHeight } from '@react-navigation/stack';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+
+import InAppBrowser from 'react-native-inappbrowser-reborn'
+import backgroundImage from "../images/background1.jpg"
+
+
 export default class ProductDetails extends Component {
     constructor(props) {
         super(props);
@@ -46,36 +51,77 @@ export default class ProductDetails extends Component {
         };
     }
     render() {
-        console.log(this.props.route.params)
-        let openURL = e => {
+        // console.log(this.props.route.params)
+        let openURL = async (e) => {
 
             Toast.show({
                 text: "Status Changed!",
                 buttonText: "Okay",
-                type: "success"
+                type: "primary"
             })
 
-            Linking.openURL(e).catch(err =>
-                Alert.alert("Invalid QRCode", e))
+            // Linking.openURL(e).catch(err =>
+            //     Alert.alert("Invalid QRCode", e))
+
+            try {
+                const url = e
+                if (await InAppBrowser.isAvailable()) {
+                    const result = await InAppBrowser.open(url, {
+                        // iOS Properties
+                        dismissButtonStyle: 'cancel',
+                        preferredBarTintColor: 'white',
+                        preferredControlTintColor: 'brown',
+                        readerMode: false,
+                        animated: true,
+                        modalPresentationStyle: 'overFullScreen',
+                        modalTransitionStyle: 'partialCurl',
+                        modalEnabled: true,
+                        enableBarCollapsing: false,
+                        // Android Properties
+
+                        showTitle: false,
+                        toolbarColor: '#5a55af',
+                        secondaryToolbarColor: 'green',
+                        enableUrlBarHiding: true,
+                        enableDefaultShare: true,
+                        forceCloseOnRedirection: false,
+                        // Specify full animation resource identifier(package:anim/name)
+                        // or only resource name(in case of animation bundled with app).
+                        animations: {
+                            startEnter: 'slide_in_right',
+                            startExit: 'slide_out_left',
+                            endEnter: 'slide_in_left',
+                            endExit: 'slide_out_right'
+                        },
+                        headers: {
+                            'my-custom-header': 'my custom header value'
+                        }
+                    })
+                    // Alert.alert(JSON.stringify(result))
+                }
+                else Linking.openURL(url)
+            } catch (error) {
+                Alert.alert(error.message)
+            }
+
         }
         return (
             <Root>
-              
-
+                <ImageBackground source={backgroundImage} style={style.image}>
                     <View style={style.container}>
-                        <View style={style.field}><Text style={style.text} > Product Name</Text><Text style={style.text}> {this.props.route.params.data.type} </Text></View>
-                        <View style={style.field}><Text style={style.text} > Product ID</Text><Text style={style.text}>{this.props.route.params.data.bounds.width} </Text></View>
-                        <View style={style.field}><Text style={style.text} > Product Price</Text><Text style={style.text}> {this.props.route.params.data.bounds.height}</Text></View>
-                        <View style={style.field}><Text style={style.text} > Product Amount</Text><Text style={style.text}> {this.props.route.params.data.target}</Text></View>
-                        <View style={style.btnContainer}><View><Button  success style={style.Btn} onPress={() => openURL(this.props.route.params.data.data)} ><Text> Change Status </Text></Button></View></View>
+                        <View><Text style={{ fontSize: 20, fontSize: RFPercentage(5), color: '#000', fontWeight: "bold" , color: "white" ,marginBottom: RFPercentage(5), }}>Ttile Here</Text></View>
+                        <View style={style.box}>
+                            <View style={style.field}><Text style={style.text} > Product Name </Text><Text style={style.text}> {this.props.route.params.data.type} </Text></View>
+                            <View style={style.field}><Text style={style.text} > Product ID </Text><Text style={style.text}> {this.props.route.params.data.bounds.width} </Text></View>
+                            <View style={style.field}><Text style={style.text} > Product Price </Text><Text style={style.text}> {this.props.route.params.data.bounds.height} </Text></View>
+                            <View style={style.field}><Text style={style.text} > Maghrib</Text><Text style={style.text}> {this.props.route.params.data.target} </Text></View>
+                            <View style={style.field}><Text style={style.text} > Product Amount </Text><Text style={style.text}> {this.props.route.params.data.target} </Text></View>
+                        </View>
+                        <View><Button style={style.VerifyBtn} onPress={() => openURL(this.props.route.params.data.data)} ><Text style={{ fontSize: 22 }}> Change  Status  </Text></Button></View>
+
                     </View>
-                 
 
-                    
-                        
-
-
-                
+                </ImageBackground>
 
             </Root>);
     }
@@ -86,9 +132,9 @@ const style = StyleSheet.create({
     container: {
         flex: 1,
         padding: 4,
-        backgroundColor: '#efefef',
         alignContent: "center",
-        justifyContent: "center"
+        justifyContent: "center",
+        alignItems: "center"
 
     },
     btnContainer: {
@@ -96,21 +142,48 @@ const style = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
 
+    }, VerifyBtn: {
+        marginTop: RFPercentage(7),
+        height: 50,
+        width: 300,
+        display: "flex",
+        justifyContent: "center"
+
+
+    },
+
+
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center"
+    },
+
+    box: {
+        margin: RFPercentage(1),
+        borderWidth: 3,
+        borderColor: "white",
+        padding: RFPercentage(0.5),
+        height: hp("37%"),
+        width: wp("85%"),
+        borderRadius: 10,
+        backgroundColor: "transparent",
+
     },
     text: {
-        fontSize: 24,
-        color: '#000'
+        fontSize: RFPercentage(3.5),
+        color: '#000',
+        color: "white"
     },
     field: {
         display: "flex",
         flexDirection: 'row',
         justifyContent: "space-between",
+        borderBottomWidth: 1,
+        paddingBottom: RFPercentage(1.5),
+        paddingTop: RFPercentage(1),
+
+        borderColor: "white"
     },
-    Btn: {
-        marginTop: RFPercentage(10),
-
-
-    }
-
 
 });
